fa72e375581846dd55b10983c696fea49f30d091
Ali Hamza <maheraliihamza565@gmail.com>
2023-05-28 15:40:30 +0500

    Updates for app responsiveness and new feature added for word count and character count

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -10,16 +10,30 @@ const Main = () => {
     const onTextChange = (event) => {
         setText(event.target.value);
     }
+    const clearText = () => {
+        setText('');
+    }
     const [text, setText] = useState('');
   return (
-    <div className="container mt-5">
-        <div className="mb-3">
-            <label htmlFor="textFormatter" className="form-label fs-1">Text Formatter</label>
-            <textarea className="form-control" value={text} id="textFormatter" rows="8" placeholder='Enter text here' onChange={onTextChange}></textarea>
+    <>
+        <div className="container mt-5">
+            <h1>Text Formatter</h1>
+            <div className="row mb-3 px-2 px-sm-0">
+                <textarea className="form-control " value={text} rows="8" placeholder='Enter text here' onChange={onTextChange}></textarea>
+            </div>
+            <div className="d-flex flex-sm-row flex-column">
+                <button type="button" className="btn btn-primary me-sm-2 mb-2" onClick={toUpperCase}>Convert to Uppercase</button>
+                <button type="button" className="btn btn-primary me-sm-2 mb-2" onClick={toLowerCase}>Convert to Lowercase</button>
+                <button type="button" className="btn btn-primary me-sm-2 mb-2" onClick={clearText}>Clear Text</button>
+            </div>
         </div>
-        <button type="button" className="btn btn-primary me-2" onClick={toUpperCase}>Convert to Uppercase</button>
-        <button type="button" className="btn btn-primary" onClick={toLowerCase}>Convert to Lowercase</button>
-    </div>
+        <div className="container my-3">
+            <h2>Your Text Summary</h2>
+            <p>{text.trim() ? text.trim().split(' ').length : 0} words and {text.length} chararcters</p>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+    </>
   )
 }
 
